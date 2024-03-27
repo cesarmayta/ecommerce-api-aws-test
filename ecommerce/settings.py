5ff7fb2b37b398a116da9c70ccf1c69cc8d55105
César Mayta <cesarmayta@gmail.com>
2024-03-27 16:49:52 -0500

    deploy 4

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-d+gn^h*c@iy&1nh1b&^lyo6$jkut927l-_b^yp+a4_z#8pdsr+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
